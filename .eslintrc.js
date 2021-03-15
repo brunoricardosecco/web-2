1026b0a2cdefdcc73720fb67d37edb45b2b1122d
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-03-14 22:08:27 -0300

    chore: add code linters

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"],
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "react/state-in-constructor": "off",
+    "react/static-property-placement": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/prefer-stateless-function": "off",
+    "react/prop-types": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-console": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+  },
+};
